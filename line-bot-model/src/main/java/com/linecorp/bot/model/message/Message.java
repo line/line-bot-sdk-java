34838378c3938fa73fe124d268132513ada52309
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-09-07 16:06:04 +0900

    Message should use @JsonTypeName for consistent interface.

--- line-bot-model/src/main/java/com/linecorp/bot/model/message/Message.java
+++ line-bot-model/src/main/java/com/linecorp/bot/model/message/Message.java
@@ -1,5 +1,22 @@
 package com.linecorp.bot.model.message;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = As.PROPERTY,
+        property = "type",
+        visible = true
+)
+@JsonSubTypes({
+                      @JsonSubTypes.Type(TextMessage.class),
+                      @JsonSubTypes.Type(ImageMessage.class),
+                      @JsonSubTypes.Type(StickerMessage.class),
+                      @JsonSubTypes.Type(LocationMessage.class),
+                      @JsonSubTypes.Type(ImageMapMessage.class),
+                      @JsonSubTypes.Type(TemplateMessage.class)
+              })
 public interface Message {
-    String getType();
 }
