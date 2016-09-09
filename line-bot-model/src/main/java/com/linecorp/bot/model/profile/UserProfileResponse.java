4a2cdbec2499ca3107d4d77aad4caf76f755e65c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-09-09 19:38:57 +0900

    remove useless comment

--- line-bot-model/src/main/java/com/linecorp/bot/model/profile/UserProfileResponse.java
+++ line-bot-model/src/main/java/com/linecorp/bot/model/profile/UserProfileResponse.java
@@ -4,22 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Value;
 
-/*
-
-{
-  "requestId": "1231feaadc...",git
-  "profiles": [
-    {
-      "displayName":"BOT API",
-      "userId":"U0047556f2e40dba2456887320ba7c76d",
-      "pictureUrl":"http://dl.profile.line.naver.jp/abcdefghijklmn",
-      "statusMessage":"Hello, LINE!"
-    },
-    ...
-  ]
-}
-
- */
 @Value
 public class UserProfileResponse {
     private final String requestId;
