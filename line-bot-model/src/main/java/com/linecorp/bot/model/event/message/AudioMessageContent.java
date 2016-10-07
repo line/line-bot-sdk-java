404ffbea700bf522bb2facfe2cc07bc55053d9dc
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-10-07 17:28:42 +0900

    Remove unused fields

--- line-bot-model/src/main/java/com/linecorp/bot/model/event/message/AudioMessageContent.java
+++ line-bot-model/src/main/java/com/linecorp/bot/model/event/message/AudioMessageContent.java
@@ -26,13 +26,9 @@ import lombok.Value;
 @JsonTypeName("audio")
 public class AudioMessageContent implements MessageContent {
     private final String id;
-    private final String url;
 
     @JsonCreator
-    public AudioMessageContent(
-            @JsonProperty("id") String id,
-            @JsonProperty("url") String url) {
+    public AudioMessageContent(@JsonProperty("id") String id) {
         this.id = id;
-        this.url = url;
     }
 }
