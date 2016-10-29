a1c4735f5a6cae658acc92c26592f29aaa1857fd
Kazuki MATSUDA <matsuda.kazuki@gmail.com>
2016-10-29 16:28:16 +0900

    Use Java8's CompletableFuture instead of Retorofit's Call.
    Motivation
    ---------
    * Use Java8's standard async mechanism.
    * Don't open implementation detail of LineMessagingClient.
      * Remove retrofit dependency from SDK outside.

---
+++ line-bot-api-client/src/main/java/com/linecorp/bot/client/LineMessagingClient.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.bot.client;
+
+import java.util.concurrent.CompletableFuture;
+
+import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.profile.UserProfileResponse;
+import com.linecorp.bot.model.response.BotApiResponse;
+
+public interface LineMessagingClient {
+    /**
+     * Reply to messages from users.
+     *
+     * <p>Webhooks are used to notify you when an event occurs. For events that you can respond to,
+     * a replyToken is issued for replying to messages.
+     * <p>Because the replyToken becomes invalid after a certain period of time,
+     * responses should be sent as soon as a message is received. Reply tokens can only be used once.
+     *
+     * @see #pushMessage(PushMessage)
+     * @see <a href="https://devdocs.line.me?java#reply-message">//devdocs.line.me#reply-message</a>
+     */
+    CompletableFuture<BotApiResponse> replyMessage(ReplyMessage replyMessage);
+
+    /**
+     * Send messages to users when you want to.
+     *
+     * <p>INFO: Use of the Push Message API is limited to certain plans.
+     *
+     * @see #replyMessage(ReplyMessage)
+     * @see <a href="https://devdocs.line.me?java#push-message">//devdocs.line.me#push-message</a>
+     */
+    CompletableFuture<BotApiResponse> pushMessage(PushMessage pushMessage);
+
+    /**
+     * Download image, video, and audio data sent from users.
+     *
+     * @see <a href="https://devdocs.line.me?java#get-content">//devdocs.line.me#get-content</a>
+     */
+    CompletableFuture<MessageContentResponse> getMessageContent(String messageId);
+
+    /**
+     * Get user profile information.
+     *
+     * @see <a href="https://devdocs.line.me?java#bot-api-get-profile">//devdocs.line.me#bot-api-get-profile</a>
+     */
+    CompletableFuture<UserProfileResponse> getProfile(String userId);
+
+    /**
+     * Leave a group.
+     *
+     * @see <a href="https://devdocs.line.me?java#leave">//devdocs.line.me#leave</a>
+     */
+    CompletableFuture<BotApiResponse> leaveGroup(String groupId);
+
+    /**
+     * Leave a room.
+     *
+     * @see <a href="https://devdocs.line.me?java#leave">//devdocs.line.me#leave</a>
+     */
+    CompletableFuture<BotApiResponse> leaveRoom(String roomId);
+}
---
+++ line-bot-api-client/src/main/java/com/linecorp/bot/client/LineMessagingClientImpl.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.bot.client;
+
+import java.util.concurrent.CompletableFuture;
+
+import com.linecorp.bot.client.exception.GeneralLineMessagingException;
+import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.profile.UserProfileResponse;
+import com.linecorp.bot.model.response.BotApiResponse;
+
+import lombok.AllArgsConstructor;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Proxy implementation of {@link LineMessagingClient} to hind internal implementation.
+ */
+@AllArgsConstructor
+public class LineMessagingClientImpl implements LineMessagingClient {
+    private static final ExceptionConverter EXCEPTION_CONVERTER = new ExceptionConverter();
+    private final LineMessagingService retrofitImpl;
+
+    @Override
+    public CompletableFuture<BotApiResponse> replyMessage(final ReplyMessage replyMessage) {
+        return toFuture(retrofitImpl.replyMessage(replyMessage));
+    }
+
+    @Override
+    public CompletableFuture<BotApiResponse> pushMessage(final PushMessage pushMessage) {
+        return toFuture(retrofitImpl.pushMessage(pushMessage));
+    }
+
+    @Override
+    public CompletableFuture<MessageContentResponse> getMessageContent(final String messageId) {
+        return toMessageContentResponseFuture(retrofitImpl.getMessageContent(messageId));
+    }
+
+    @Override
+    public CompletableFuture<UserProfileResponse> getProfile(final String userId) {
+        return toFuture(retrofitImpl.getProfile(userId));
+    }
+
+    @Override
+    public CompletableFuture<BotApiResponse> leaveGroup(final String groupId) {
+        return toFuture(retrofitImpl.leaveGroup(groupId));
+    }
+
+    @Override
+    public CompletableFuture<BotApiResponse> leaveRoom(final String roomId) {
+        return toFuture(retrofitImpl.leaveRoom(roomId));
+    }
+
+    private static <T> CompletableFuture<T> toFuture(Call<T> callToWrap) {
+        final CallbackAdaptor<T> completableFuture = new CallbackAdaptor<>();
+        callToWrap.enqueue(completableFuture);
+        return completableFuture;
+    }
+
+    private static CompletableFuture<MessageContentResponse> toMessageContentResponseFuture(
+            final Call<ResponseBody> callToWrap) {
+        final ResponseBodyCallbackAdaptor future = new ResponseBodyCallbackAdaptor();
+        callToWrap.enqueue(future);
+        return future;
+    }
+
+    static class CallbackAdaptor<T> extends CompletableFuture<T> implements Callback<T> {
+        @Override
+        public void onResponse(final Call<T> call, final Response<T> response) {
+            if (response.isSuccessful()) {
+                complete(response.body());
+            } else {
+                completeExceptionally(EXCEPTION_CONVERTER.apply(response));
+            }
+        }
+
+        @Override
+        public void onFailure(final Call<T> call, final Throwable t) {
+            completeExceptionally(
+                    new GeneralLineMessagingException(t.getMessage(), null, t));
+        }
+    }
+
+    static class ResponseBodyCallbackAdaptor
+            extends CompletableFuture<MessageContentResponse>
+            implements Callback<ResponseBody> {
+
+        @Override
+        public void onResponse(final Call<ResponseBody> call, final Response<ResponseBody> response) {
+            if (!response.isSuccessful()) {
+                completeExceptionally(EXCEPTION_CONVERTER.apply(response));
+                return;
+            }
+
+            try {
+                complete(convert(response));
+            } catch (RuntimeException exceptionInConvert) {
+                completeExceptionally(
+                        new GeneralLineMessagingException(exceptionInConvert.getMessage(),
+                                                          null, exceptionInConvert));
+            }
+        }
+
+        @Override
+        public void onFailure(final Call<ResponseBody> call, final Throwable t) {
+            completeExceptionally(
+                    new GeneralLineMessagingException(t.getMessage(), null, t));
+        }
+
+        private MessageContentResponse convert(final Response<ResponseBody> response) {
+            return MessageContentResponse
+                    .builder()
+                    .length(response.body().contentLength())
+                    .allHeaders(response.headers().toMultimap())
+                    .mimeType(response.body().contentType().toString())
+                    .stream(response.body().byteStream())
+                    .build();
+        }
+    }
+}
