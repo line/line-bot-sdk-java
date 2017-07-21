b3aba14814ed7c14ca5829e2ec24d0eb1acf1f7e
Kevin Wang <kevinw@cse.ust.hk>
2017-07-21 16:39:01 +0800

    test again

---
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DatabaseEngine.java
@@ -0,0 +1,42 @@
+package com.example.bot.spring;
+
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DatabaseEngine {
+	String search(String text) throws Exception {
+		String result = null;
+		BufferedReader br = null;
+		FileReader fr = null;
+		try {
+			fr = new FileReader(FILENAME);
+   		    br = new BufferedReader(fr);
+			String sCurrentLine;
+			while (result != null && (sCurrentLine = br.readLine()) != null) {
+				String[] parts = sCurrentLine.split(":");
+				if (text.toLowerCase().equals(parts[0].toLowerCase())) {
+					result = parts[1];
+				}
+			}
+		} catch (IOException e) {
+			log.info("IOException while reading file: {}", e.toString());
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+				if (fr != null)
+					fr.close();
+			} catch (IOException ex) {
+				log.info("IOException while closing file: {}", ex.toString());
+			}
+		}
+		if (result != null)
+			return result;
+		throw new Exception("NOT FOUND");
+    }
+	private final String FILENAME = "/static/database.txt";
+}
