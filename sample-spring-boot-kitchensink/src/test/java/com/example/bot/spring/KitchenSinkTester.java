9d6767e2c2dedf5b0520164888efa6ef439c2238
Kevin Wang <khwang0@gmail.com>
2017-09-19 10:48:06 +0800

    Fix to Issue #2
    See issue #2

--- sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
+++ sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
@@ -59,7 +59,7 @@ public class KitchenSinkTester {
 		} catch (Exception e) {
 			thrown = true;
 		}
-		assertThat(thrown);
+		assertThat(thrown).isEqualTo(true);
 	}
 	
 	@Test
@@ -71,7 +71,7 @@ public class KitchenSinkTester {
 		} catch (Exception e) {
 			thrown = true;
 		}
-		assertThat(!thrown);
-		assertThat(result.equals("def"));
+		assertThat(!thrown).isEqualTo(true);
+		assertThat(result).isEqualTo("def");
 	}
 }
