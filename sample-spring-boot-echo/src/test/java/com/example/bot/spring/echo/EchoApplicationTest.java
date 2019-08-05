aaf9fab6253fd1bfa5bcbb6f67e40571d38c3d99
Masahiro Ide <masahiro.ide@linecorp.com>
2019-08-05 16:51:07 +0900

    Add a unit test to check validity of sample app's spring context
    Closes #211

---
+++ sample-spring-boot-echo/src/test/java/com/example/bot/spring/echo/EchoApplicationTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.example.bot.spring.echo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Test for checking validity of sample-spring-boot-echo's Spring context.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class EchoApplicationTest {
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    public void test() {
+        assertThat(context).isNotNull();
+    }
+}
