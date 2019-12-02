570ad4654bfb6438beeefb74a243a8d7fa5f1b46
Okue <nogideca@gmail.com>
2019-12-02 10:40:03 +0900

    NON-ISSUE Refactoring sample-spring-boot (#409)
    * use logger
    
    * remove compile
    
    * use org.slf4j.LoggerFactory

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -16,6 +16,8 @@
 
 package com.example.bot.spring.echo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -30,13 +32,15 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 @SpringBootApplication
 @LineMessageHandler
 public class EchoApplication {
+    private final Logger log = LoggerFactory.getLogger(EchoApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(EchoApplication.class, args);
     }
 
     @EventMapping
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        System.out.println("event: " + event);
+        log.info("event: " + event);
         final String originalMessageText = event.getMessage().getText();
         return new TextMessage(originalMessageText);
     }
