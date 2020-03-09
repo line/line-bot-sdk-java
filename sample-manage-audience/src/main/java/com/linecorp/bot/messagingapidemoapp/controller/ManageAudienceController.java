9a70ae46077133b89f36658c99eca0fa876ce22d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-03-09 15:13:29 +0900

    Rename audience group

--- sample-manage-audience/src/main/java/com/linecorp/bot/messagingapidemoapp/controller/ManageAudienceController.java
+++ sample-manage-audience/src/main/java/com/linecorp/bot/messagingapidemoapp/controller/ManageAudienceController.java
@@ -1,7 +1,15 @@
 package com.linecorp.bot.messagingapidemoapp.controller;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
+import com.linecorp.bot.model.manageaudience.request.Audience;
+import com.linecorp.bot.model.manageaudience.request.CreateAudienceGroupRequest;
+import com.linecorp.bot.model.manageaudience.request.UpdateAudienceGroupDescriptionRequest;
+import com.linecorp.bot.model.manageaudience.response.CreateAudienceGroupResponse;
+import io.micrometer.core.instrument.util.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -59,4 +67,52 @@ public class ManageAudienceController {
 
         return "manage_audience/show"; // show src/main/resources/templates/manage_audience/quota.ftlh
     }
+
+    @GetMapping("/manage_audience/upload")
+    public String upload() {
+        return "manage_audience/upload";
+    }
+
+    @PostMapping("/manage_audience/upload")
+    public RedirectView postUpload(
+            @RequestParam String description,
+            @RequestParam Boolean isIfaAudience,
+            @RequestParam(required = false) String uploadDescription,
+            @RequestParam String audiences
+            ) throws ExecutionException, InterruptedException {
+        List<Audience> audiencesList = Arrays.stream(audiences.split("\r?\n"))
+                .map(it -> it.replaceAll("\\s", ""))
+                .filter(StringUtils::isNotBlank)
+                .map(Audience::new)
+                .collect(Collectors.toList());
+
+        CreateAudienceGroupRequest request = CreateAudienceGroupRequest.builder()
+                .description(description)
+                .isIfaAudience(isIfaAudience)
+                .uploadDescription(uploadDescription)
+                .audiences(audiencesList)
+                .build();
+        CreateAudienceGroupResponse response = client.createAudienceGroup(request).get();
+
+        return new RedirectView("/manage_audience/" + response.getAudienceGroupId());
+    }
+
+    @GetMapping("/manage_audience/updateDescription/{audienceGroupId}")
+    public String updateDescription(@PathVariable Long audienceGroupId,
+                                    Model model) throws ExecutionException, InterruptedException {
+        GetAudienceDataResponse response = client.getAudienceData(audienceGroupId).get();
+        model.addAttribute("audienceGroup", response.getAudienceGroup());
+        return "manage_audience/updateDescription";
+    }
+
+    @PostMapping("/manage_audience/updateDescription/{audienceGroupId}")
+    public RedirectView postUpdateDescription(@PathVariable Long audienceGroupId,
+                                              @RequestParam String description) throws ExecutionException, InterruptedException {
+        UpdateAudienceGroupDescriptionRequest request = UpdateAudienceGroupDescriptionRequest.builder()
+                .description(description)
+                .build();
+        client.updateAudienceGroupDescription(audienceGroupId, request).get();
+        return new RedirectView("/manage_audience/" + audienceGroupId);
+    }
 }
+
