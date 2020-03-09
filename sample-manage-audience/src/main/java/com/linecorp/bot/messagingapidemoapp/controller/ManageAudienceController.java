a06d85381c8a462bdf952821c00271c0f438e3d0
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-03-09 15:58:32 +0900

    respect line's oss coding standard

--- sample-manage-audience/src/main/java/com/linecorp/bot/messagingapidemoapp/controller/ManageAudienceController.java
+++ sample-manage-audience/src/main/java/com/linecorp/bot/messagingapidemoapp/controller/ManageAudienceController.java
@@ -22,11 +22,6 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
-import com.linecorp.bot.model.manageaudience.request.Audience;
-import com.linecorp.bot.model.manageaudience.request.CreateAudienceGroupRequest;
-import com.linecorp.bot.model.manageaudience.request.UpdateAudienceGroupDescriptionRequest;
-import com.linecorp.bot.model.manageaudience.response.CreateAudienceGroupResponse;
-import io.micrometer.core.instrument.util.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,10 +31,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.manageaudience.request.Audience;
+import com.linecorp.bot.model.manageaudience.request.CreateAudienceGroupRequest;
+import com.linecorp.bot.model.manageaudience.request.UpdateAudienceGroupDescriptionRequest;
+import com.linecorp.bot.model.manageaudience.response.CreateAudienceGroupResponse;
 import com.linecorp.bot.model.manageaudience.response.GetAudienceDataResponse;
 import com.linecorp.bot.model.manageaudience.response.GetAudienceGroupsResponse;
 import com.linecorp.bot.model.response.BotApiResponse;
 
+import io.micrometer.core.instrument.util.StringUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -56,7 +56,7 @@ public class ManageAudienceController {
     ) throws ExecutionException, InterruptedException {
         GetAudienceGroupsResponse audienceGroups
                 = client.getAudienceGroups(page, null, null, 40L, null,
-                null)
+                                           null)
                         .get();
         model.addAttribute("audienceGroups", audienceGroups);
 
@@ -96,19 +96,19 @@ public class ManageAudienceController {
             @RequestParam Boolean isIfaAudience,
             @RequestParam(required = false) String uploadDescription,
             @RequestParam String audiences
-            ) throws ExecutionException, InterruptedException {
+    ) throws ExecutionException, InterruptedException {
         List<Audience> audiencesList = Arrays.stream(audiences.split("\r?\n"))
-                .map(it -> it.replaceAll("\\s", ""))
-                .filter(StringUtils::isNotBlank)
-                .map(Audience::new)
-                .collect(Collectors.toList());
+                                             .map(it -> it.replaceAll("\\s", ""))
+                                             .filter(StringUtils::isNotBlank)
+                                             .map(Audience::new)
+                                             .collect(Collectors.toList());
 
         CreateAudienceGroupRequest request = CreateAudienceGroupRequest.builder()
-                .description(description)
-                .isIfaAudience(isIfaAudience)
-                .uploadDescription(uploadDescription)
-                .audiences(audiencesList)
-                .build();
+                                                                       .description(description)
+                                                                       .isIfaAudience(isIfaAudience)
+                                                                       .uploadDescription(uploadDescription)
+                                                                       .audiences(audiencesList)
+                                                                       .build();
         CreateAudienceGroupResponse response = client.createAudienceGroup(request).get();
 
         return new RedirectView("/manage_audience/" + response.getAudienceGroupId());
@@ -124,10 +124,12 @@ public class ManageAudienceController {
 
     @PostMapping("/manage_audience/updateDescription/{audienceGroupId}")
     public RedirectView postUpdateDescription(@PathVariable Long audienceGroupId,
-                                              @RequestParam String description) throws ExecutionException, InterruptedException {
+                                              @RequestParam String description)
+            throws ExecutionException, InterruptedException {
         UpdateAudienceGroupDescriptionRequest request = UpdateAudienceGroupDescriptionRequest.builder()
-                .description(description)
-                .build();
+                                                                                             .description(
+                                                                                                     description)
+                                                                                             .build();
         client.updateAudienceGroupDescription(audienceGroupId, request).get();
         return new RedirectView("/manage_audience/" + audienceGroupId);
     }
