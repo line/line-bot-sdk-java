1c8562db67b4a34d22e4e733a8b5eef41b2be6d7
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-11-14 09:58:56 +0900

    limit.upToRemainingQuota property has been added to narrowcast messages. #572

--- line-bot-model/src/main/java/com/linecorp/bot/model/narrowcast/Limit.java
+++ line-bot-model/src/main/java/com/linecorp/bot/model/narrowcast/Limit.java
@@ -35,6 +35,12 @@ public class Limit {
      */
     Integer max;
 
+    /**
+     * If true, the message will be sent within the maximum number of deliverable messages. The default value is false.
+     * Targets will be selected at random.
+     */
+    Boolean upToRemainingQuota;
+
     @JsonPOJOBuilder(withPrefix = "")
     public static class LimitBuilder {
         // Filled by lombok
