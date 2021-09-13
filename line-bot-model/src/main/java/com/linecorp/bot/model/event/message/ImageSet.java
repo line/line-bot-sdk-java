1d65becae6d73a60624f1aa1d84396a1561a5394
Hiroki Nishino <7100187+tawAsh1@users.noreply.github.com>
2021-09-13 11:59:43 +0900

    Support multi image metadata (#694)

--- line-bot-model/src/main/java/com/linecorp/bot/model/event/message/ImageSet.java
+++ line-bot-model/src/main/java/com/linecorp/bot/model/event/message/ImageSet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 LINE Corporation
+ * Copyright 2021 LINE Corporation
  *
  * LINE Corporation licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
@@ -16,27 +16,35 @@
 
 package com.linecorp.bot.model.event.message;
 
-import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
 import lombok.Builder;
 import lombok.Value;
 
-/**
- * Message content for image type.
- */
-@JsonTypeName("image")
 @Value
 @Builder(toBuilder = true)
-@JsonDeserialize(builder = ImageMessageContent.ImageMessageContentBuilder.class)
-public class ImageMessageContent implements MessageContent {
+@JsonDeserialize(builder = ImageSet.ImageSetBuilder.class)
+public class ImageSet {
     @JsonPOJOBuilder(withPrefix = "")
-    public static class ImageMessageContentBuilder {
+    public static class ImageSetBuilder {
         // Providing builder instead of public constructor. Class body is filled by lombok.
     }
 
+    /**
+     * Image set ID. Only included when multiple images are sent simultaneously.
+     */
     String id;
-    ContentProvider contentProvider;
-    ImageSet imageSet;
+
+    /**
+     * An index starting from 1, indicating the image number in a set of images sent simultaneously.
+     * Only included when multiple images are sent simultaneously.
+     */
+    Integer index;
+
+    /**
+     * The total number of images sent simultaneously.
+     * Only included when multiple images are sent simultaneously.
+     */
+    Integer total;
 }
