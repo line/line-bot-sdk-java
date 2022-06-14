26df327cadf4a4ee22ef5784d7f800c2c4097b33
kcvlex <krkr9893@gmail.com>
2022-06-14 17:55:07 +0900

    Fix code style

--- line-bot-model/src/main/java/com/linecorp/bot/model/action/PostbackAction.java
+++ line-bot-model/src/main/java/com/linecorp/bot/model/action/PostbackAction.java
@@ -16,8 +16,12 @@
 
 package com.linecorp.bot.model.action;
 
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 import lombok.Builder;
 import lombok.Value;
--- line-bot-model/src/test/java/com/linecorp/bot/model/action/PostbackSerializeTest.java
+++ line-bot-model/src/test/java/com/linecorp/bot/model/action/PostbackSerializeTest.java
@@ -1,20 +1,39 @@
+/*
+ * Copyright 2022 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.linecorp.bot.model.action;
 
 import static java.lang.ClassLoader.getSystemResourceAsStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.linecorp.bot.model.objectmapper.ModelObjectMapper;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.linecorp.bot.model.objectmapper.ModelObjectMapper;
+
+import lombok.SneakyThrows;
+
 public class PostbackSerializeTest {
-    private final static ObjectMapper OBJECT_MAPPER = ModelObjectMapper.createNewObjectMapper();
+    private static final ObjectMapper OBJECT_MAPPER = ModelObjectMapper.createNewObjectMapper();
 
     @SneakyThrows
     private JsonNode readResource(String name) {
-        return OBJECT_MAPPER.readTree(getSystemResourceAsStream(name));
+        return PostbackSerializeTest.OBJECT_MAPPER.readTree(getSystemResourceAsStream(name));
     }
 
     @Test
@@ -28,7 +47,7 @@ public class PostbackSerializeTest {
                 null
         );
 
-        final JsonNode node1 = OBJECT_MAPPER.valueToTree(action);
+        final JsonNode node1 = PostbackSerializeTest.OBJECT_MAPPER.valueToTree(action);
         final JsonNode node2 = readResource("action/postback_richmenu.json");
 
         assertEquals(node1, node2);
@@ -45,7 +64,7 @@ public class PostbackSerializeTest {
                 "FILLINTEXT"
         );
 
-        final JsonNode node1 = OBJECT_MAPPER.valueToTree(action);
+        final JsonNode node1 = PostbackSerializeTest.OBJECT_MAPPER.valueToTree(action);
         final JsonNode node2 = readResource("action/postback_keyboard.json");
 
         assertEquals(node1, node2);
