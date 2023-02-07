57b053a4339fc78595d6c4fb8a9747d2fdf29297
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-02-07 13:36:07 +0900

    Bump checkstyle 8.24 to 8.25. JavaMethodCheck: minLineCount was deprecated. https://github.com/checkstyle/checkstyle/issues/7096

--- build.gradle.kts
+++ build.gradle.kts
@@ -117,7 +117,7 @@ subprojects {
         configProperties = mapOf(
             "projectDir" to rootProject.projectDir.toString(),
         )
-        toolVersion = "8.24"
+        toolVersion = "8.25"
     }
 
     tasks.withType(com.github.spotbugs.snom.SpotBugsTask::class.java) {
