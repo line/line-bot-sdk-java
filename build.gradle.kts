01b853be8dfc2114c8c6307373be28cb6b113d1e
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-02-07 13:39:19 +0900

    Bump checkstyle 8.27 to 8.28
    Remove deprecated props.
    https://github.com/checkstyle/checkstyle/issues/7413

--- build.gradle.kts
+++ build.gradle.kts
@@ -117,7 +117,7 @@ subprojects {
         configProperties = mapOf(
             "projectDir" to rootProject.projectDir.toString(),
         )
-        toolVersion = "8.27"
+        toolVersion = "8.28"
     }
 
     tasks.withType(com.github.spotbugs.snom.SpotBugsTask::class.java) {
