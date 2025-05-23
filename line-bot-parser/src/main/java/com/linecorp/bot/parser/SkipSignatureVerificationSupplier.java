fce264e6099dd2d48d2046e70f0f020783c9bb0c
habara keigo <habara.keigo@linecorp.com>
2025-05-23 17:53:10 +0900

    Add option to skip signature verification

---
+++ line-bot-parser/src/main/java/com/linecorp/bot/parser/SkipSignatureVerificationSupplier.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2025 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.bot.parser;
+
+import java.util.function.BooleanSupplier;
+
+/**
+ * Special {@link BooleanSupplier} for Skip Signature Verification.
+ *
+ * <p>You can implement it to return whether to skip signature verification.
+ */
+@FunctionalInterface
+public interface SkipSignatureVerificationSupplier extends BooleanSupplier {
+}
